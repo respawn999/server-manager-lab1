cbcd3ff4c9ec86da940dd5f011b110b1dc381cbf
Filip K. <102953124+respawn999@users.noreply.github.com>
2023-09-28 22:15:04 +0100

    Added comments to clarify code

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from manager import *
 
 def main():
+    # Defining number of servers as per requirements
     server_number = 10
+    # List comprehension to create Server objects and put them into a list
     server_list = [Server(5) for i in range(server_number)]
 
 
--- manager.py
+++ manager.py
@@ -2,43 +2,46 @@ import random
 import time
 
 class Server:
-
+    # initilisation for the Server class
     def __init__(self, c):
         self.c = c
         self.started = False
         self.last_active = time.time()
 
+    # Checking if server has capacity, labelled as c following requirements
     def has_c(self, workload):
         return self.started and (self.c-workload) >= 0
     
+    # Function to start server
     def started(self):
         self.started = True
         self.last_active = time.time()
 
+    # Function to stop server
     def stop(self):
         self.started = False
 
+    # Function to label a server idle if inactive for more than 5 time units
     def idle(self):
         return not self.started and (time.time() - self.last_active) <= 5
     
 
 class Job:
-
+    # initilising Job class
     def __init__(self, id, workload):
         self.id = id
         self.workload = workload
 
-
+    # Function to create a queue as a list, add jobs, assign ids and add created jobs onto the queue
     def jobQueue(self,id,workload):
         queue = []
         for i in range(1,51):
             workload = random.randint(1,20)
             job = Job(i, workload)
+            # As per requirements, delay between jobs is implemented, with the wait time being a random time unit bwtween 2 and 5
             time.sleep(random.randint(2,5))
+            # Adding job onto queue list
             job.append(queue)
-
-        for i in queue:
-            print(i)
         
 
 
